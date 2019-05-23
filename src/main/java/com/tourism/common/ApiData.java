2dacdace09bd3eb80da566790672345eee07105a
mac <8824247231@qq.com>
2019-05-23 11:52:21 +0800

    自动化配置showapi 增加默认Windows和Mac版本logback日志配置文件

--- src/main/java/com/tourism/common/ApiData.java
+++ src/main/java/com/tourism/common/ApiData.java
@@ -1,11 +1,19 @@
 package com.tourism.common;
 
 import com.show.api.ShowApiRequest;
+import org.springframework.beans.factory.annotation.Value;
 
 public class ApiData {
 
+    @Value("#{configProperties['showApi.url']}")
+    private String showApiUrl;
+    @Value("#{configProperties['showApi.appid']}")
+    private String showApiAppid;
+    @Value("#{configProperties['showApi.appSecret']}")
+    private String showApiAppSecret;
+
     public String getApiData(String keyword){
-        String res = new ShowApiRequest("http://route.showapi.com/268-1","92031","d1ddc4dfb8514260aa13efdba71f9e8f")
+        String res = new ShowApiRequest(showApiUrl, showApiAppid, showApiAppSecret)
                 .addTextPara("keyword",keyword)
                 .addTextPara("proId","")
                 .addTextPara("cityId","")
